82d730212601caa0dfb47ed7d6f934019bc8347f
Yogesh Jangir <yogeshjangir1902@gmail.com>
2025-07-08 17:41:58 +0530

    Add files via upload

---
+++ litral_constant.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 19;
+    float pi = 3.14;
+    char grade = 'A';
+    string name = "Yogesh";
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Pi value: " << pi << endl;
+    cout << "Are you a student? " << isStudent << endl;
+
+    return 0;
+}
