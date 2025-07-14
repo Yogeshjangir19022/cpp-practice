284aa19f78fdd90f0c21f9cad46b63f5de4c4dda
Yogesh Jangir <yogeshjangir1902@gmail.com>
2025-07-14 23:40:49 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int age ;
+std ::cout <<"What is your age?  ";
+std::cin >> age ;
+switch(age ){
+
+case 13:
+std ::cout<< "Your age is 13 \n";
+break;
+case 14 :
+std::cout << "Your age is 14 \n";
+break;
+case 15 :
+std ::cout << "Your age is 15 \n";
+break;
+default:
+std ::cout<< "All done ";
+break;
+
+}
+
+    return 0;
+}
+
